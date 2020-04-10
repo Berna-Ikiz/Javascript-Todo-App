70e3fceb1d97e6a0f68f2b7054caa80199247a41
Berna Ikiz <brn.ikiz.35@gmail.com>
2020-04-10 19:01:18 +0300

    add delete item event

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
 //UI variables
-const form=document.querySelector('form');
-const input=document.querySelector('#txtTaskName');
+const form = document.querySelector('form');
+const input = document.querySelector('#txtTaskName');
 
-const btnDeleteAll=document.querySelector('#btnDeleteAll');
+const btnDeleteAll = document.querySelector('#btnDeleteAll');
 const taskList = document.querySelector('#task-list');
 
 eventListeners();
 
 
 //call event Listeners
-function eventListeners()
-{
+function eventListeners() {
     //Submit Event
-   form.addEventListener('submit',addNewItem);   
+    form.addEventListener('submit', addNewItem);
+    //delete an Item  
+    taskList.addEventListener('click', deleteItem);
+
+    //delete All Items
+    btnDeleteAll.addEventListener('click', deleteAllItems)
 }
 
 //Add new item
-function addNewItem(e){
-    
-    if(input.value===''){
+function addNewItem(e) {
+
+    if (input.value === '') {
         alert('add new item')
+    } else {
+        //create li
+        const li = document.createElement('li');
+        li.className = 'list-group-item list-group-item-secondary';
+        li.appendChild(document.createTextNode(input.value));
+
+        //create a
+        const a = document.createElement('a');
+        a.classList = 'delete-item float-right';
+        a.setAttribute('href', '#')
+        a.innerHTML = '<i class="fas fa-times"></i>';
+
+        //add a to li
+        li.appendChild(a);
+
+        //add li to ul
+        taskList.appendChild(li)
+
+        //clear input
+        input.value = '';
+        console.log(li);
     }
-    else{
-    //create li
-    const li=document.createElement('li');
-    li.className = 'list-group-item list-group-item-secondary';
-    li.appendChild(document.createTextNode(input.value));
-
-    //create a
-    const a=document.createElement('a');     
-    a.classList = 'delete-item float-right';
-    a.setAttribute('href','#')
-    a.innerHTML='<i class="fas fa-times"></i>';
-    
-    //add a to li
-    li.appendChild(a);
-    
-    //add li to ul
-    taskList.appendChild(li)
-
-    //clear input
-    input.value='';
-    console.log(li);}
     e.preventDefault()
 }
 
+
+function deleteItem(e) {
+    if (confirm('Are you sure')) {
+        if (e.target.className === 'fas fa-times') {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+    e.preventDefault();
+}
+
+function deleteAllItems(e) {
+    if (confirm('are you sure?')) {
+        taskList.innerHTML = '';
+    }
+    e.preventDefault();
+}
